c92d94b89dfbabd1f66ed3d3ec0d8013ec40f7bf
lucasFreitasDev97 <lukasm2@live.com>
2024-07-25 18:52:08 -0300

    structure of method in class

--- src/AnatomyClasses.java
+++ src/AnatomyClasses.java
@@ -1,5 +1,11 @@
 public class AnatomyClasses {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("My Name is".concat(completeName(" Lucas", "Freitas")));
     }
+
+   public static String completeName(String firstName, String lastName)
+   {
+       return firstName.concat(" ").concat(lastName);
+   }
 }
