54b86d1332b11d5909c40b25a615edd0004b871c
lucasFreitasDev97 <lukasm2@live.com>
2024-07-25 18:56:23 -0300

    refactory code

--- src/AnatomyClasses.java
+++ src/AnatomyClasses.java
@@ -1,11 +1,14 @@
 public class AnatomyClasses {
     public static void main(String[] args) {
+        String firstName = "Lucas";
+        String lastName = "Freitas";
+
         System.out.println("Hello world!");
-        System.out.println("My Name is".concat(completeName(" Lucas", "Freitas")));
+        System.out.println(completeName(firstName, lastName));
     }
 
    public static String completeName(String firstName, String lastName)
    {
-       return firstName.concat(" ").concat(lastName);
+       return "My name is " + firstName + " " + lastName;
    }
 }
