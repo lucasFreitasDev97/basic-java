74a91afb328ca6abbf95b58364fd8c930c71ae22
lucasFreitasDev97 <lukasm2@live.com>
2024-07-29 16:12:37 -0300

    remove empty spaces

--- src/OperatorClass.java
+++ src/OperatorClass.java
@@ -2,13 +2,13 @@ public class OperatorClass {
     public static void main(String[] args) {
         int number01 = 500;
         int number02 = 340;
+
         //===============ARITHMETICS===============//
         int sumValue = number01 + number02; //  -> +
         int subtractionValue = number01 - number02; // -> -
         int multiplicationValue = number01 * number02; // -> *
         int moduleValue = number01 % number02; // -> %
         double divisionValue = (double) number01 / (double) number02; // -> /
-
 
         System.out.println("=====================");
         System.out.println("Arithmetics Operators");
